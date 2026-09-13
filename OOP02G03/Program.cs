a510ee2fe25932d3d24b3f797c3f43d3e9ae031f
passant ahmed <passantahmed992006@outlook.com>
2026-09-13 19:48:25 +0300

    Add project files.

---
+++ OOP02G03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OOP02G03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01
+            //Class refrance tybe , heap , pass by reference , يدعم التوريث من  class  تاني , ممكن يبقي ب null 
+
+            //struct value type , stack , pass by value , لا يدعم التوريث من struct تاني , لا يمكن ان يكون ب null
+            #endregion
+        }
+    }
+}
