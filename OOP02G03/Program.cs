e484e560793f38400e2205b7e1fe0e0037a97156
passant ahmed <passantahmed992006@outlook.com>
2026-09-13 20:01:13 +0300

    Q02

--- OOP02G03/Program.cs
+++ OOP02G03/Program.cs
@@ -11,6 +11,11 @@ namespace OOP02G03
 
             //struct value type , stack , pass by value , لا يدعم التوريث من struct تاني , لا يمكن ان يكون ب null 
             #endregion
+            #region 02
+            //a) parent class : shipment ,  b) child class:ExpressShipment , c) members are inherited by ExpressShipment :Trackingcode ,
+            //d) Why is inheritance better than duplicating the same code in multiple classes?
+            //     reason: Inheritance promotes code reusability and maintainability.By creating a parent class with common functionality, child classes can inherit that functionality without duplicating code.
+            #endregion
         }
-    }
+}
 }
