74ba0ecc0a85f69c234d21fa873f47720d418aa5
passant ahmed <passantahmed992006@outlook.com>
2026-09-13 19:49:15 +0300

    Q01

--- OOP02G03/Program.cs
+++ OOP02G03/Program.cs
@@ -9,7 +9,7 @@ namespace OOP02G03
             #region 01
             //Class refrance tybe , heap , pass by reference , يدعم التوريث من  class  تاني , ممكن يبقي ب null 
 
-            //struct value type , stack , pass by value , لا يدعم التوريث من struct تاني , لا يمكن ان يكون ب null
+            //struct value type , stack , pass by value , لا يدعم التوريث من struct تاني , لا يمكن ان يكون ب null 
             #endregion
         }
     }
